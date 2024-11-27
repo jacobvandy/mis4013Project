ceae6c2ad082417118a16bf498a5166490b6d2d2
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-11-27 12:55:51 -0600

    Create model-candy.php

---
+++ model-candy.php
@@ -0,0 +1,58 @@
+<?php
+function SelectCandy() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT CandyID, Name, Type, Price, ManufacturerID FROM candy");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function insertCandy($cName, $cType, $cPrice, cManufacturerID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO candy (Name, Type, Price, ManufacturerID) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssdi", $cName, $cType, $cPrice, $cManufacturerID);
+        $success =  $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateCandy($cName, $cType, $cPrice, cManufacturerID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE candy SET Name = ?, Type = ?, Price = ?, ManudacturerID = ? WHERE CandyID = ?");
+        $stmt->bind_param("ssdi", $cName, $cType, $cPrice, cManufacturerID);
+        $success =  $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteCandy($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM candy WHERE CandyID = ?");
+        $stmt->bind_param("i", $cid);
+        $success =  $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>

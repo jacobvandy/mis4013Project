fcca8845cea7cf4a89ed5f4a7396bd48f109ebb6
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-11-27 17:17:50 -0600

    Update model-manufacturer-by-candy.php

--- model-manufacturer-by-candy.php
+++ model-manufacturer-by-candy.php
@@ -2,7 +2,7 @@
 function SelectManufacturerByCandy($mid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT m.ManufacturerID, m.Name, m.CountryItemName, c.Name, c.Price FROM candy c join manufacturer m on c.ManufacturerID = m.ManufacturerID WHERE c.CandyID = ?");
+        $stmt = $conn->prepare("SELECT m.ManufacturerID, m.Name, m.Country, c.Name, c.Price FROM candy c join manufacturer m on c.ManufacturerID = m.ManufacturerID WHERE c.CandyID = ?");
         $stmt->bind_param("i", $mid);
         $stmt->execute();
         $result = $stmt->get_result();

11d52780060eaed6d19eec8b3669b0a0f65913b0
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-11-27 17:15:54 -0600

    Create model-manufacturer-by-candy.php

---
+++ model-manufacturer-by-candy.php
@@ -0,0 +1,16 @@
+<?php
+function SelectManufacturerByCandy($mid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT m.ManufacturerID, m.Name, m.CountryItemName, c.Name, c.Price FROM candy c join manufacturer m on c.ManufacturerID = m.ManufacturerID WHERE c.CandyID = ?");
+        $stmt->bind_param("i", $mid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>

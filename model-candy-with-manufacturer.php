b2eafa39adecaec5b30e604cfb20b06bd3d8cabb
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-11-29 10:15:40 -0600

    Update model-candy-with-manufacturer.php

--- model-candy-with-manufacturer.php
+++ model-candy-with-manufacturer.php
@@ -2,7 +2,7 @@
 function SelectCandy() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT RestaurantID, Name, Rating, FoodType FROM tacobell");
+        $stmt = $conn->prepare("SELECT CandyID, Name, Type, Price, ManufacturerID FROM candy");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();

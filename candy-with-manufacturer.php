febf6c75b569cb05ff5347c890b2e8a1f95ec0ba
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-12-10 14:46:36 -0600

    Update candy-with-manufacturer.php

--- candy-with-manufacturer.php
+++ candy-with-manufacturer.php
@@ -4,6 +4,40 @@ require_once("model-candy-with-manufacturer.php");
 
 $pageTitle = "Manufacturers and their Candies";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+ switch ($_POST['actionType']) {
+   case "Add":
+   if (insertCanManu($_POST['mName'], $_POST['mCountry'], $_POST['mid'], $_POST['cName'], $_POST['cPrice'], $_POST['cid'])) {
+    echo '<div class="alert alert-success" role="alert">Cany and Manu Added</div>';
+   } else {
+    '<div class="alert alert-danger" role="alert">Error adding Candy w/ Manufacturer</div>';
+   }
+  
+   break;
+
+
+  case "Edit":
+   if (updateCanManu($_POST['mName'], $_POST['mCountry'])) {
+    echo '<div class="alert alert-success" role="alert"> Candy edited!</div>';
+   } else {
+    '<div class="alert alert-danger" role="alert">Error editing Candy</div>';
+   }
+   break;
+  
+
+  case "Delete":
+   if (deleteCanManu($_POST['cid'])) {
+    echo '<div class="alert alert-success" role="alert">Candy Deleted</div>';
+   } else {
+    '<div class="alert alert-danger" role="alert">Error deleting Candy</div>';
+   }
+  
+   break;
+ }
+}
+
+
 $candy = SelectCandy();
 include "view-candy-with-manufacturer.php";
 include "view-footer.php";

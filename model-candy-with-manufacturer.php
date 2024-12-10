0c7ae67c905e5f80cbdee08c4d7c945af34119c5
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-12-10 12:24:14 -0600

    Update model-candy-with-manufacturer.php

--- model-candy-with-manufacturer.php
+++ model-candy-with-manufacturer.php
@@ -27,4 +27,70 @@ function SelectManufacturerByCandy($mid) {
         throw $e;
     }
 }
+
+function SelectManuForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT ManufacturerID, Name FROM manufacturer ORDER BY Name" );
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertCanManu($mName, $mCountry, $cName, $cPrice) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO manufacturer (Name, Country) VALUES (?, ?)");
+        $stmt->bind_param("ss", $mName, $mCountry);
+        $success =  $stmt->execute();
+
+        $stmt = $conn->prepare("INSERT INTO candy (Name, Price) VALUES (?,?)");
+        $stmt->bind_param("sd", $cName, $cPrice);
+        $success =  $stmt->execute();
+        
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateCanManu($mName, $mCountry $cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE manufacturer SET Name = ?, Country = ? WHERE CandyID = ?");
+        $stmt->bind_param("ssi", $mName, $mCountry, $cid);
+        $success =  $stmt->execute();
+
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteCanManu($manid) {
+    try {
+        $conn = get_db_connection();
+      
+         $stmt = $conn->prepare("DELETE FROM manufacturer WHERE ManufacturerID = ?");
+        $stmt->bind_param("i", $manid);
+        $success =  $stmt->execute();
+
+        
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
